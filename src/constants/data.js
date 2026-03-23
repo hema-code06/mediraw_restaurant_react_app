d0089bc52b937cde28f1c8b477693b70fd0963c1
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-23 20:00:23 +0530

    data and images are added

---
+++ src/constants/data.js
@@ -0,0 +1,73 @@
+import images from './images';
+
+const coastline = [
+  {
+    title: 'Paella Valenciana',
+    price: '₹156',
+  },
+  {
+    title: 'Linguine alle Vongole',
+    price: '₹256',
+  },
+  {
+    title: 'Greek-Style Grilled Octopus',
+    price: '₹556',
+  },
+  {
+    title: 'Moroccan Fish Tagine',
+    price: '₹156',
+  },
+  {
+    title: 'Turkish Seafood Pilaf',
+    price: '₹556',
+  },
+  {
+    title: 'French Bouillabaisse',
+    price: '₹356',
+  },
+  {
+    title: 'Portuguese Grilled Sardines',
+    price: '₹856',
+  },
+];
+
+const mezze = [
+  {
+    title: 'Hummus',
+    price: '₹856',
+  },
+  {
+    title: 'Baba Ganoush',
+    price: '₹156',
+  },
+  {
+    title: 'Tabbouleh',
+    price: '₹756',
+  },
+  {
+    title: 'Dolmas',
+    price: '₹356',
+  },
+  {
+    title: 'Labneh',
+    price: '₹256',
+  },
+  {
+    title: 'Tzatziki',
+    price: '₹556',
+  },
+  {
+    title: 'Falafel',
+    price: '₹656',
+  },
+  {
+    title: 'Muhammara',
+    price: '₹356',
+  },
+  {
+    title: 'Saganaki',
+    price: '₹256',
+  },
+];
+
+export default { coastline, mezze };
---
+++ src/constants/images.js
@@ -0,0 +1,50 @@
+import bg from '../assets/bg.png';
+import chef from '../assets/cc.png';
+import gallery01 from '../assets/G1.jpg';
+import gallery02 from '../assets/G2.jpg';
+import gallery03 from '../assets/G3.jpg';
+import gallery04 from '../assets/G4.jpg';
+import gallery05 from '../assets/G5.jpg';
+import gallery06 from '../assets/G6.jpg';
+import gallery07 from '../assets/G7.jpg';
+import gallery08 from '../assets/G8.png';
+import gallery09 from '../assets/G9.jpg';
+import gallery10 from '../assets/G10.jpg';
+import gallery11 from '../assets/G11.jpg';
+import gallery12 from '../assets/G12.jpg';
+import gallery13 from '../assets/G13.jpg';
+import gallery14 from '../assets/G14.jpg';
+import menu from '../assets/Menu.jpg';
+import welcome from '../assets/Home.png';
+import findus from '../assets/C.png';
+import sign from '../assets/sign.png';
+import Medit from '../assets/Medit.png';
+import Footer from '../assets/L.png';
+import spn from '../assets/spn.png';
+
+
+export default {
+  bg,
+  chef,
+  Footer,
+  spn,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  gallery05,
+  gallery06,
+  gallery07,
+  gallery08,
+  gallery09,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  menu,
+  welcome,
+  findus,
+  sign,
+  Medit,
+};
